28431f10f20e7baec3d30a5803a2fd0ec84f7046
Simone Tarantino <tarantinosimonew@gmail.com>
2020-03-30 21:41:03 +0200

    View guest

---
+++ resources/views/Posts/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+    @foreach ($posts as $post)
+        <table class="table">
+            <thead>
+                <th >
+                    Article ID:
+                </th>
+                <th>
+                    Title:
+                </th>
+                <th>
+                    Writer ID:
+                </th>
+                <th>
+                    Created At:
+                </th>
+                <th>
+                    Updated At:
+                </th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        {{$post->id}}
+                    </td>
+                    <td>
+                        {{$post->title}}
+                    </td>
+                    <td>
+                        {{$post->user_id}}
+                    </td>
+                    <td>
+                        {{$post->created_at}}
+                    </td>
+                    <td>
+                        {{$post->updated_at}}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    @endforeach  
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('posts', 'PostController@index')->name('index');
 
 Auth::routes();
 
